08c93ead372d4ff732e681ad460bc78747866a51
Chunduru <aschunduru@galaxe.com>
2024-01-04 15:28:33 +0530

    modified the import statement

--- index.ts
+++ index.ts
@@ -23,9 +23,7 @@ import {
   GetObjectCommandOutput
 } from '@aws-sdk/client-s3';
 
-import xml-js;
-// import xmlJs;
-// from 'xml-js';
+import xmlJs from 'xml-js';
 import * as _ from "lodash";
 
 const client = new S3Client({ region: "us-east-1" });
