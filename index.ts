86273ed6de1b69c3372a85674ec26fe14935cf57
Chunduru <aschunduru@galaxe.com>
2024-01-05 16:15:51 +0530

    modified

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 // import { APIGatewayProxyEvent, APIGatewayProxyResultV2, Handler } from 'aws-lambda';
 // import * as _ from 'lodash';
-
 // export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> => {
 //   const max = 200;
 //   const val = _.random(max);
@@ -10,15 +9,10 @@
 //   };
 //   return response;
 // };
-
-// import { promises as fsPromises } from 'fs'; 
-// import { parseString } from 'xml2js';
-
 import { 
   SQSEvent, 
   SQSHandler
 } from 'aws-lambda';
-
 import {
   S3Client,
   GetObjectCommandInput,
@@ -28,11 +22,9 @@ import {
 
 import xmlJs from 'xml-js';
 import * as _ from "lodash";
-
 const client = new S3Client({ region: "us-east-1" });
 const file_name = "Sample_xml_processingfile.xml"
 const bucket = "ak-workouts-us-east-1"
-
 async function getObject(key: string): Promise<GetObjectCommandOutput> {
   // const appId = `${svcName} | getObject`;
   const params: GetObjectCommandInput = {
@@ -40,7 +32,6 @@ async function getObject(key: string): Promise<GetObjectCommandOutput> {
       Key: file_name,
   };
   const command = new GetObjectCommand(params);
-
   try {
       return await client.send(command);
   } catch (error) {
@@ -48,34 +39,25 @@ async function getObject(key: string): Promise<GetObjectCommandOutput> {
       throw error;
   }
 }
-
 async function getObjectAsString(key: string): Promise<string> {
   try {
       const s3Object = await getObject(key);
-
       if (!s3Object.Body) {
           throw new Error(`No objectstring found for ${key}, ${bucket}`);
       }
-
       return await s3Object.Body.transformToString();
   } catch (error) {
     console.log(error.message, { key, bucket }, error);
       throw error;
   }
 }
-
-
 export function parseWebStore(webStore: string): number {
 	return parseInt(webStore.replace(/\D/g, ''), 10);
 }
-
-
 export function getFirstTextXml(item: any): string {
 	if (!item || !item[0] || !item[0]._text || !item[0]._text[0]) return '';
 	return item[0]._text[0].trim();
 }
-
-
 export function parseFulfillmentXml(xml: string, filename: string) {
 	const appId = 'parseFulfillmentXml';
 	try {
@@ -86,13 +68,16 @@ export function parseFulfillmentXml(xml: string, filename: string) {
 
 		// this should never happen
 		// if (!json?.Data945) {
+		// if (!json?.Data945) {
 		if (!json) {
 			throw new Error(`parseFulfillmentXml: malformed xml file ${filename}`);
 		}
 
 		return {
+			webStore: parseWebStore(getFirstTextXml(json.Data945[0].WebStore)),
 			webStore: parseWebStore(getFirstTextXml(json[0].WebStore)),
 			// we need to drill down a few levels to get to the '<document>' node
+			fulfillments: json.Data945[0].documents[0].document.map(document => {
 			fulfillments: json[0].documents[0].document.map(document => {
 				const header = document.headerrow[0];
 				return {
@@ -121,25 +106,14 @@ export function parseFulfillmentXml(xml: string, filename: string) {
 		throw error;
 	}
 }
-
-
-
 export const handler: SQSHandler = async (event: SQSEvent) => {
   
   try {
-      const xmlbody = getObjectAsString(file_name);
-      console.log(`The file body is:: ${xmlbody}`)
-	  const { fulfillments, webStore: newWebStore } = parseFulfillmentXml(xmlbody, file_name);
-	//   parseString(xmlbody, function (err, results) { 
-	// 	// parsing to json 
-	// 	let data = JSON.stringify(results)   
-	// 	// display the json data 
-	// 	console.log("results",data); 
-	// 	});
+      const body = getObjectAsString(file_name);
+      console.log(`The file body is:: ${body}`)
   }
   catch (err) {
       console.log(err);
       throw err;
-
   }
 };
