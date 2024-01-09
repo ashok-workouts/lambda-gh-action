48a8beb6338f045c01c519f179c726b81f415681
Chunduru <aschunduru@galaxe.com>
2024-01-09 18:07:56 +0530

    Changed for SQS connection

--- index.ts
+++ index.ts
@@ -11,9 +11,16 @@
 //   return response;
 // };
 
-import { SQSService } from '@sqs';
-// import { promises as fsPromises } from 'fs'; 
-// import { parseString } from 'xml2js';
+// import { SQSService } from '@sqs';
+// import {SQSService} from 'sqs';
+import {
+	MessageAttributeValue,
+	SendMessageBatchCommand,
+	SendMessageBatchCommandInput,
+	SendMessageCommand,
+	SendMessageCommandInput,
+	SQSClient,
+} from '@aws-sdk/client-sqs';
 
 import { 
 	SQSEvent, 
@@ -26,7 +33,9 @@ import {
 	GetObjectCommand,
 	GetObjectCommandOutput
   } from '@aws-sdk/client-s3';
-  
+
+import PQueue from 'p-queue';
+import { v4 as uuidv4 } from 'uuid';
 import xmlJs from 'xml-js';
 import * as _ from "lodash";
   
@@ -34,6 +43,8 @@ const client = new S3Client({ region: "us-east-1" });
 // const file_name = "Sample_xml_processingfile.xml"
 const file_name = "Sample_945ECom_E102.xml"
 const bucket = "ak-workouts-us-east-1"
+const AWS_REGION = 'us-east-1';
+const _sqsClient = new SQSClient({ region: AWS_REGION });
   
 async function getObject(key: string): Promise<GetObjectCommandOutput> {
 	console.log("getObject() started execution........")
@@ -81,6 +92,24 @@ export function getFirstTextXml(item: any): string {
 	if (!item || !item[0] || !item[0]._text || !item[0]._text[0]) return '';
 	return item[0]._text[0].trim();
 }
+
+
+export interface SendParams {
+	message: string;
+	messageAttributes?: Record<string, MessageAttributeValue>;
+	id?: string;
+	delaySeconds?: number;
+	messageDeduplicationId?: string;
+	messageGroupId?: string;
+}
+
+
+interface SQSServiceResponse {
+	publish: (publishParams: SendParams) => Promise<void>;
+	publishBatch: (messages: string[]) => Promise<void>;
+	chunkAndPublish: (messages: string[]) => Promise<void>;
+}
+
 
 export interface PackageLineItem {
 	SKU: string;
@@ -212,6 +241,111 @@ export function parseFulfillmentXml(xml: string, filename: string): ParsedFulfil
 		console.log(appId, `${filename} ${error.message}`, { filename }, error);
 		throw error;
 	}
+}
+
+export function SQSService(queueArn?: string): SQSServiceResponse {
+	const svcName = `🗼 SQSService`;
+
+	if (!AWS_REGION) {
+		// Should never happen as this is a standard Lambda env var
+		throw new Error('AWS_REGION');
+	}
+	const CHUNK_SIZE = 10;
+	const PROMISE_QUEUE = new PQueue({ concurrency: 500 });
+
+	const _sqsEndpointUrl = _sqsClient.config.endpointProvider({ Region: AWS_REGION }).url.href;
+
+	function _deconstructQueueArn() {
+		if (!queueArn) {
+			throw new Error();
+		}
+		const accountId = queueArn.split(':')[4];
+		const queueName = queueArn.split(':')[5];
+		return `${_sqsEndpointUrl}${accountId}/${queueName}`;
+	}
+
+	async function publish(publishParams: SendParams) {
+		const appId = `${svcName} | publish`;
+		const queueUrl = _deconstructQueueArn();
+		const {
+			message,
+			messageAttributes,
+			id,
+			delaySeconds,
+			messageDeduplicationId,
+			messageGroupId,
+		} = publishParams;
+		const logParams = { id, queueUrl, message, messageAttributes, queueArn };
+
+		const params: SendMessageCommandInput = {
+			QueueUrl: queueUrl,
+			MessageBody: message,
+		};
+
+		if (messageAttributes) {
+			params.MessageAttributes = messageAttributes;
+		}
+		if (delaySeconds) {
+			params.DelaySeconds = Math.round(delaySeconds);
+		}
+		if (messageDeduplicationId) {
+			params.MessageDeduplicationId = messageDeduplicationId;
+		}
+		if (messageGroupId) {
+			params.MessageGroupId = messageGroupId;
+		}
+		try {
+			const command = new SendMessageCommand(params);
+			await _sqsClient.send(command);
+		} catch (error) {
+			console.log(appId, error.message, logParams, error);
+			throw error;
+		}
+	}
+
+	async function publishBatch(messages: string[]) {
+		const appId = `${svcName} | publishBatch`;
+		const queueUrl = _deconstructQueueArn();
+		const params: SendMessageBatchCommandInput = {
+			QueueUrl: queueUrl,
+			Entries: [],
+		};
+
+		for (const message of messages) {
+			params.Entries?.push({
+				Id: uuidv4(),
+				MessageBody: message,
+			});
+		}
+
+		try {
+			const command = new SendMessageBatchCommand(params);
+			const result = await _sqsClient.send(command);
+			console.log(appId, 'SQS Result', result);
+		} catch (error) {
+			console.log(appId, error.message, {}, error);
+			throw error;
+		}
+	}
+
+	async function chunkAndPublish(messages: string[]) {
+		const chunks: string[][] = [];
+
+		let i, j, temparray;
+		for (i = 0, j = messages.length; i < j; i += CHUNK_SIZE) {
+			temparray = messages.slice(i, i + CHUNK_SIZE);
+			chunks.push(temparray);
+		}
+		for (const chunkitems of chunks) {
+			PROMISE_QUEUE.add(async () => {
+				await publishBatch(chunkitems);
+			});
+		}
+
+		await PROMISE_QUEUE.onIdle();
+	}
+
+	return { publish, publishBatch, chunkAndPublish };
 }
 
 
